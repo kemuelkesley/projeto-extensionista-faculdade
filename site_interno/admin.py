5931da8ccf24927b259141d71547ec28afd908e2
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2024-05-30 08:21:35 -0300

    Criada a visualização na area administrativa.

--- site_interno/admin.py
+++ site_interno/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from . models import Operacional, Sobre
 
-admin.site.register(Operacional)
-admin.site.register(Sobre)
+
+class OperacionalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'activate', 'created_at')   
+    list_filter = ('title', 'activate', 'created_at')
+    date_hierarchy = 'created_at'
+    ordering = ('title', 'created_at')   
+    readonly_fields = ('created_at',)
+
+
+
+class SobreAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    ordering = ('description', 'created_at')   
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(Operacional, OperacionalAdmin)
+admin.site.register(Sobre, SobreAdmin)
 
 
--- site_interno/models.py
+++ site_interno/models.py
@@ -5,8 +5,8 @@ class Operacional(models.Model):
     title = models.CharField(max_length=60, blank=False, null=False)
     descripiton = models.TextField(max_length=200)
     image = models.ImageField(upload_to='imagens_operacionais/')
-    activate = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    activate = models.BooleanField(verbose_name='Ativar', default=True)
+    created_at = models.DateTimeField(verbose_name='Data de publicação',auto_now_add=True)
     
     class Meta:
         verbose_name_plural = 'Operacional'
@@ -27,12 +27,12 @@ class Operacional(models.Model):
 class Sobre(models.Model):
     description = models.TextField(max_length=1000, null=True)
     image_company = models.ImageField(upload_to='imagens_sobre/')
-    activate = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    activate = models.BooleanField(verbose_name='Ativar',default=True)
+    created_at = models.DateTimeField(verbose_name='Data de publicação',auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'Sobre'
 
     def __str__(self):
-        return f"Descrição da Página Sobre - {self.description[:25]}"    
+        return f"Descrição da Página Sobre - {self.description[:35]}"    
     
